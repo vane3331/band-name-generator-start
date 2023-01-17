5ab016d3bb7e3d9aaa89bfac35c0c0cfcb671062
Vanessa Sanchez <122924756+vane3331@users.noreply.github.com>
2023-01-17 19:31:29 +0000

    Initial commit

---
+++ Band.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+street = input("What city did you grow up in?\n ")
+pet = input("What is the name of your pet?\n ")
+print("Your band name can be " + street +" " + pet)
+
+
+
